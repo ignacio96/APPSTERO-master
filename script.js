0da55f442d205a33cf486bc8867ee92962773ad8
simbe23 <simbe23@users.noreply.github.com>
2018-08-28 15:58:27 -0300

    Top navbar sticky

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	setBindings();
+
+
+
+function setBindings() {
+
+	$(".navbar a").click(function(e){
+		e.preventDefault();
+		var sectionID = e.currentTarget.id + "1";
+
+		$("html body").animate({
+			scrollTop: $("#" + sectionID).offset().top
+		}, 1000)
+	})};
+
+	$(".details").click(function(e){
+		$(".imgBox").css("opacity", 0.2);
+	});
+
+
+
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("mynav");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+});
